7220459c0716b6222f4c8353407b2329f8a211b2
bart.chang <bart.chang@tm-robot.com>
2020-06-04 15:05:44 +0800

    add fun list

--- tm_driver/include/tm_driver/tm_command.h
+++ tm_driver/include/tm_driver/tm_command.h
@@ -65,30 +65,82 @@ public:
 	}
 
 	////////////////////////////////
-	// Fucntions
+	// Functions
 	////////////////////////////////
 
+/* 
+	Leaving external control mode.
+	More Detail please refer to the TM_Robot_Expression.pdf Chapter 8.2 */
 	static std::string script_exit() { return "ScriptExit()"; }
+	
+//	More details please refer to the TM_Robot_Expression.pdf Chapter 9.1
 	static std::string set_tag(int tag, int wait = 0);
+
+//	More details please refer to the TM_Robot_Expression.pdf Chapter 9.2
 	static std::string set_wait_tag(int tag, int timeout_ms = 0);
+
+/*	Stopping robot motion and clear buffer.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.3 */
 	static std::string set_stop() { return "StopAndClearBuffer()"; }
+
+/*	Pausing pjoject and robot motion.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.4 */	
 	static std::string set_pause() { return "Pause()"; }
+
+/*	Resuming project operation and robot motion.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.5 */
 	static std::string set_resume() { return "Resume()"; }
+
+/*	Triggering IO high/low.
+	TmIOModule type: ControlBox, EndEffector 
+	TmIOType   type: DI, DO, InstantDO, AI, AO, InstantAO 
+	More details please refer to the TM_Robot_Expression.pdf Chapter 7.5*/	
 	static std::string set_io(TmIOModule module, TmIOType type, int pin, float state);
+	
+/*	PTP(point to point) motion with angle calculation.
+	The motion is mot always a straight line.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.6 */	
 	static std::string set_joint_pos_PTP(const std::vector<double> &angs,
 		int vel_percent, double acc_time, int blend_percent, bool fine_goal, int precision = 5);
+	
+/*	PTP(point to point) motion with tool calculation.
+	The motion is mot always a straight line.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.6 */	
 	static std::string set_tool_pose_PTP(const std::vector<double> &pose,
 		int vel_percent, double acc_time, int blend_percent, bool fine_goal, int precision = 5);
+	
+/*	Linear motion with tool calculation.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.7 */	
 	static std::string set_tool_pose_Line(const std::vector<double> &pose,
 		double vel, double acc_time, int blend_percent, bool fine_goal, int precision = 5);
 
+/*	PVT start.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.16 */
 	static std::string set_pvt_enter(int mode) { return "PVTEnter(" + std::to_string(mode) + ")"; }
+	
+/*	PVT leave.
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.17 */	
 	static std::string set_pvt_exit() { return "PVTExit()"; }
+	
+/*	Setting target, velocity and time.
+	TmPvtMode :	Joint, Tool
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.18 */	
 	static std::string set_pvt_point(TmPvtMode mode,
 		double t, const std::vector<double> &pos, const std::vector<double> &vel, int precision = 5);
+	
+/*	Setting target, velocity and time.
+	TmPvtMode :	Joint, Tool
+	TmPvtPoint:	time ,positions and velocities;
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.18 */
 	static std::string set_pvt_point(TmPvtMode mode, const TmPvtPoint &point, int precision = 5)
 	{
 		return set_pvt_point(mode, point.time, point.positions, point.velocities, precision);
 	}
+
+/*	Setting target, velocity and total time.
+	TmPvtTraj 	: TmPvtMode mode
+	points		: target
+	total_time	: total time
+	More details please refer to the TM_Robot_Expression.pdf Chapter 9.18 */
 	static std::string set_pvt_traj(const TmPvtTraj &pvts, int precision = 5);
 };
