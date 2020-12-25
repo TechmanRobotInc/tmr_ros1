49039bf7a64a7e5e63abff33f8f881aacc694e72
ken.tsai <ken.tsai@tm-robot.com>
2020-12-25 16:10:54 +0800

    build item vector at first parsing

--- tm_driver/include/tm_driver/tm_robot_state.h
+++ tm_driver/include/tm_driver/tm_robot_state.h
@@ -6,10 +6,17 @@
 #include <vector>
 #include <string>
 #include <mutex>
+#include <functional>
 
+
+class TmDataTable;
 
 class TmRobotState
 {
+	friend class TmDataTable;
+private:
+	TmDataTable *_data_table;
+
 public:
 	enum { DOF = 6 };
 
@@ -100,6 +107,16 @@ private:
 	std::vector<unsigned char> _ee_DI;
 	std::vector<float> _ee_AO;
 	std::vector<float> _ee_AI;
+
+private:
+	std::function<size_t (void *, const char *, size_t)> _f_deserialize_item[2];
+	std::function<size_t (const char *, size_t, bool)> _f_deserialize;
+	struct ItemUpdate {
+		void *dst;
+		size_t func;
+		enum { SKIP, UPDATE };
+	};
+	std::vector<ItemUpdate> _item_updates;
 
 public:
 	TmRobotState();
@@ -203,17 +220,21 @@ private:
 	}
 
 private:
+	static size_t _deserialize_get_name(std::string &name, const char *data, size_t offset);
+	static size_t _deserialize_skip(void *dst, const char *data, size_t offset);
 	static size_t _deserialize_copy_wo_check(void *dst, const char *data, size_t offset);
+	size_t _deserialize_first_time(const char *data, size_t size, bool lock);
 	size_t _deserialize(const char *data, size_t size, bool use_mtx);
+	void _deserialize_update(bool lock);
 
 public:
 	size_t deserialize(const char *data, size_t size)
 	{
-		return _deserialize(data, size, false);
+		return _f_deserialize(data, size, false);
 	}
 	size_t mtx_deserialize(const char *data, size_t size)
 	{
-		return _deserialize(data, size, true);
+		return _f_deserialize(data, size, true);
 	}
 
 	void print();
